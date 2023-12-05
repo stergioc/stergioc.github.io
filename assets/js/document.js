391d0e05ae01f5d997f5b1ac9f56152b48c3cb7f
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2023-12-05 14:23:50 +0100

    added positions

--- assets/js/document.js
+++ assets/js/document.js
@@ -1,43 +1,40 @@
-$(document).ready(function()
-{
-    if (top.location.pathname === '/publications/')
-    {
+$(document).ready(function () {
+    if (top.location.pathname === '/publications/') {
         var x = new XMLHttpRequest();
         // Change the link to the appropriate one.
         x.open("GET", "https://dblp.org/pid/167/1074.xml", true);
         x.onreadystatechange = function () {
-            if (x.readyState == 4 && x.status == 200)
-            {
+            if (x.readyState == 4 && x.status == 200) {
                 var publications = x.responseXML;
                 var r = publications.getElementsByTagName("r");
                 var html_str = '<ul>';
-                
-                for(var i = 0; i < r.length; i++) {
+
+                for (var i = 0; i < r.length; i++) {
                     html_str += "<li>"
                     console.log(r[i].innerHTML);
 
                     var authors = r[i].getElementsByTagName('author');
                     var authors_str = ''
-                    for(var j = 0; j < authors.length; j++) {
+                    for (var j = 0; j < authors.length; j++) {
                         authors_str += authors[j].innerHTML
-                        if (j < authors.length-1) {
+                        if (j < authors.length - 1) {
                             authors_str += ', '
                         } else {
                             authors_str += ';'
                         }
                     }
-                    html_str += "<i>"+authors_str+"</i>"
+                    html_str += authors_str
 
                     var title_str = " " + r[i].getElementsByTagName('title')[0].innerHTML;
                     var doi = r[i].getElementsByTagName('ee')[0].innerHTML;
 
-                    html_str += "<strong> <a href='"+doi+"' target='_blank'>" + title_str + "</a></strong>;"
+                    html_str += "<strong> <i> <a href='" + doi + "' target='_blank'>" + title_str + "</a> </i> </strong>;"
 
                     var journal = r[i].getElementsByTagName('journal')[0];
                     if (!journal) {
                         var journal = r[i].getElementsByTagName('booktitle')[0];
                     }
-                    html_str += " "+journal.innerHTML + " ";
+                    html_str += " " + journal.innerHTML + " ";
 
                     var year = r[i].getElementsByTagName('year')[0];
                     html_str += year.innerHTML + '.';
@@ -50,6 +47,6 @@ $(document).ready(function()
         };
         x.send(null);
     }
-    
-    
+
+
 });
---
+++ positions.md
@@ -0,0 +1,17 @@
+---
+layout: page
+title: Positions
+permalink: /positions/
+---
+
+We are always looking for highly motivated and qualified applicants who are 
+interested in joining our research group. Experience in computer science, 
+artificial intelligence and medical data computing is favorable.  
+
+Currently available open possitions:
+
+<div id='positions'>
+    <ul>
+        <li><a href="/assets/pdf/MICS-Stage.pdf" target="_blank">Research Internship Position</a></li>
+    </ul>
+</div>
